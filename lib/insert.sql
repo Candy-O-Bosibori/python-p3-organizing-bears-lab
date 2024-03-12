d3c1791cdd4a19008aa9cbb55e7860723454826f
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-03-12 19:04:46 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 30, "F", "brown", "calm", 1),
+(2,"Rowdy", 15, "M", "pink", "intense", 0),
+(3,"Tabitha", 36, "M", "red", "Nice", 1),
+(4,"Sergeant Brown", 59, "F", "Orange", "Slimy", 0),
+(5,"Melissa", 17, "F", "Yellow", "goofy", 1),
+(6,"Grinch", 333, "M", "Green", "Grinchy", 0),
+(7,"Wendy", 77, "F", "Blue", "naive", 1),
+(8,null, 2, "F", "purple", "aggressive", 0);
